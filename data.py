2aaa65cbed138170113e11227524384bc4f358fd
alexalaver <alex.alaverdyan.04@gmail.com>
2023-07-21 02:00:54 +0400

    new_vers

---
+++ data.py
@@ -0,0 +1,75 @@
+import psycopg2
+
+class DataBase:
+    def __init__(self, host, port, data, user, password):
+        self.connect = psycopg2.connect(
+            host=host,
+            port=port,
+            database=data,
+            user=user,
+            password=password
+        )
+        self.cursor = self.connect.cursor()
+
+    def add_user(self, user_id, first_name, username):
+        with self.connect:
+            self.cursor.execute("INSERT INTO users(user_id, first_name, username) VALUES(%s, %s, %s)", (user_id, first_name, username,))
+            self.connect.commit()
+
+    def check_user(self, user_id):
+        with self.connect:
+            self.cursor.execute("SELECT user_id FROM users WHERE user_id=%s", (user_id,))
+            return bool(len(self.cursor.fetchall()))
+
+    def add_queue(self, chat_id):
+        with self.connect:
+            self.cursor.execute("INSERT INTO queue(chat_id) VALUES(%s)", (chat_id,))
+            self.connect.commit()
+
+    def delete_queue(self, chat_id):
+        with self.connect:
+            self.cursor.execute("DELETE FROM queue WHERE chat_id=%s", (chat_id,))
+            self.connect.commit()
+
+    def get_queue(self, user_id):
+        with self.connect:
+            self.cursor.execute("SELECT chat_id FROM queue WHERE chat_id=%s", (user_id,))
+            return bool(len(self.cursor.fetchall()))
+
+    def get_user_queue(self):
+        with self.connect:
+            self.cursor.execute("SELECT chat_id FROM queue")
+            user = self.cursor.fetchone()
+            if user is not None and bool(len(user)):
+                return user[0]
+            else:
+                return False
+
+    def create_chat(self, chat_one, chat_two):
+        with self.connect:
+            if chat_two != 0:
+                self.cursor.execute("DELETE FROM queue WHERE chat_id=%s", (chat_two,))
+                self.cursor.execute("INSERT INTO chats(chat_one, chat_two) VALUES(%s, %s)", (chat_one, chat_two,))
+                self.connect.commit()
+                return True
+            else:
+                return False
+
+    def delete_chat(self, user_id):
+        with self.connect:
+            self.cursor.execute("DELETE FROM chats WHERE chat_one=%s OR chat_two=%s", (user_id, user_id,))
+            self.connect.commit()
+
+
+    def get_active_chat(self, user_id):
+        with self.connect:
+            self.cursor.execute("SELECT * FROM chats WHERE chat_one=%s OR chat_two=%s", (user_id, user_id,))
+            users = self.cursor.fetchone()
+            if users is not None:
+                if users[1] == user_id:
+                    return users[2]
+                elif users[2] == user_id:
+                    return users[1]
+            else:
+                return False
+
---
+++ main.py
@@ -0,0 +1,147 @@
+from aiogram import Bot, Dispatcher, executor, types
+from data import DataBase
+# from aiogram.dispatcher.filters.state import State, StatesGroup
+# from aiogram.dispatcher.storage import FSMContext
+# from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import config as cfg
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(cfg.TOKEN)
+dp = Dispatcher(bot)
+db = DataBase('localhost', '5432', 'anonchat', 'anon_user', 'anon_pwd')
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    if message.chat.type == types.ChatType.PRIVATE:
+        if(not db.check_user(message.from_user.id)):
+            db.add_user(message.from_user.id, message.from_user.first_name, message.from_user.username)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        button1 = types.KeyboardButton(cfg.SEARCH)
+        markup.add(button1)
+        await message.answer(cfg.START, reply_markup=markup)
+
+@dp.message_handler(commands=['stop'])
+async def stop(message: types.Message):
+    if message.chat.type == types.ChatType.PRIVATE:
+        chat_info = db.get_active_chat(message.from_user.id)
+        if chat_info != False:
+            db.delete_chat(message.from_user.id)
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+            button1 = types.KeyboardButton(cfg.SEARCH)
+            markup.add(button1)
+            await dp.bot.send_message(message.from_user.id, cfg.STOP_DIALOG_TEXT, reply_markup=markup)
+            await dp.bot.send_message(chat_info, cfg.STOP_DIALOG_TEXT_SOBESEDNIK, reply_markup=markup)
+        else:
+            await message.answer(cfg.CANCEl_STOP_DIALOG_TEXT)
+
+@dp.message_handler(commands=['next'])
+async def next(message: types.Message):
+    if message.chat.type == types.ChatType.PRIVATE:
+        chat_info = db.get_active_chat(message.from_user.id)
+        if chat_info != False:
+            db.delete_chat(message.from_user.id)
+            markups = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+            buttons1 = types.KeyboardButton(cfg.SEARCH)
+            markups.add(buttons1)
+            await dp.bot.send_message(chat_info, cfg.STOP_DIALOG_TEXT_SOBESEDNIK, reply_markup=markups)
+
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+            button1 = types.KeyboardButton(cfg.STOP_SEARCH)
+            markup.add(button1)
+
+            chat_two = db.get_user_queue()
+
+            if db.create_chat(message.from_user.id, chat_two) == False:
+                db.add_queue(message.from_user.id)
+                await message.answer(cfg.SEARCH_PROCESS, reply_markup=markup)
+            else:
+                markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+                buttons1 = types.KeyboardButton(cfg.SEARCH_DRUGOGO)
+                buttons2 = types.KeyboardButton(cfg.STOP_DIALOG)
+                markup1.add(buttons1, buttons2)
+                await dp.bot.send_message(message.from_user.id, cfg.SEARCH_TRUE, reply_markup=markup1)
+                await dp.bot.send_message(chat_two, cfg.SEARCH_TRUE, reply_markup=markup1)
+        else:
+            await message.answer(cfg.CANCEl_STOP_DIALOG_TEXT)
+
+@dp.message_handler(content_types=['text'])
+async def text(message: types.Message):
+    if message.chat.type == types.ChatType.PRIVATE:
+        if message.text == cfg.SEARCH:
+            chat_info = db.get_active_chat(message.from_user.id)
+            if chat_info == False:
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+                button1 = types.KeyboardButton(cfg.STOP_SEARCH)
+                markup.add(button1)
+
+                chat_two = db.get_user_queue()
+
+                if db.create_chat(message.from_user.id, chat_two) == False:
+                    db.add_queue(message.from_user.id)
+                    await message.answer(cfg.SEARCH_PROCESS, reply_markup=markup)
+                else:
+                    markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+                    buttons1 = types.KeyboardButton(cfg.SEARCH_DRUGOGO)
+                    buttons2 = types.KeyboardButton(cfg.STOP_DIALOG)
+                    markup1.add(buttons1, buttons2)
+                    await dp.bot.send_message(message.from_user.id, cfg.SEARCH_TRUE, reply_markup=markup1)
+                    await dp.bot.send_message(chat_two, cfg.SEARCH_TRUE, reply_markup=markup1)
+            else:
+                await message.answer(cfg.CANCEL_TEXT)
+        elif message.text == cfg.STOP_DIALOG:
+            chat_info = db.get_active_chat(message.from_user.id)
+            if chat_info != False:
+                db.delete_chat(message.from_user.id)
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+                button1 = types.KeyboardButton(cfg.SEARCH)
+                markup.add(button1)
+                await dp.bot.send_message(message.from_user.id, cfg.STOP_DIALOG_TEXT, reply_markup=markup)
+                await dp.bot.send_message(chat_info, cfg.STOP_DIALOG_TEXT_SOBESEDNIK, reply_markup=markup)
+            else:
+                await message.answer(cfg.CANCEl_STOP_DIALOG_TEXT)
+        elif message.text == cfg.STOP_SEARCH:
+            if db.get_queue(message.from_user.id):
+                db.delete_queue(message.from_user.id)
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+                button1 = types.KeyboardButton(cfg.SEARCH)
+                markup.add(button1)
+                await message.answer(cfg.STOP_SEARCH_TEXT, reply_markup=markup)
+            else:
+                await message.answer(cfg.CANCEl_STOP_DIALOG_TEXT)
+        elif message.text == cfg.SEARCH_DRUGOGO:
+            chat_info = db.get_active_chat(message.from_user.id)
+            if chat_info != False:
+                db.delete_chat(message.from_user.id)
+                markups = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+                buttons1 = types.KeyboardButton(cfg.SEARCH)
+                markups.add(buttons1)
+                await dp.bot.send_message(chat_info, cfg.STOP_DIALOG_TEXT_SOBESEDNIK, reply_markup=markups)
+
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+                button1 = types.KeyboardButton(cfg.STOP_SEARCH)
+                markup.add(button1)
+
+                chat_two = db.get_user_queue()
+
+                if db.create_chat(message.from_user.id, chat_two) == False:
+                    db.add_queue(message.from_user.id)
+                    await message.answer(cfg.SEARCH_PROCESS, reply_markup=markup)
+                else:
+                    markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+                    buttons1 = types.KeyboardButton(cfg.SEARCH_DRUGOGO)
+                    buttons2 = types.KeyboardButton(cfg.STOP_DIALOG)
+                    markup1.add(buttons1, buttons2)
+                    await dp.bot.send_message(message.from_user.id, cfg.SEARCH_TRUE, reply_markup=markup1)
+                    await dp.bot.send_message(chat_two, cfg.SEARCH_TRUE, reply_markup=markup1)
+            else:
+                await message.answer(cfg.CANCEl_STOP_DIALOG_TEXT)
+        else:
+            chat_info = db.get_active_chat(message.from_user.id)
+            if chat_info != False:
+                await dp.bot.send_message(chat_info, message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
