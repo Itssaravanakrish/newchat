196a335b3f61cf10314c502e1ffd9ec851ccb2ac
alexalaver <alex.alaverdyan.04@gmail.com>
2023-07-28 01:07:53 +0400

    versioon

--- main.py
+++ main.py
@@ -99,16 +99,13 @@ async def get_admin_channels(channel_usernames):
     return admin_channels
 
 #check subcribed user in channel
-async def check_user_subscription(channel_username):
+async def check_user_subscription(channel_username, user_id):
     try:
-        channel_info = await bot.get_chat(f"@{channel_username}")
-        channel_id = channel_info.id
-
-        # Получаем количество участников канала
-        chat_member_count = await bot.get_chat_member_count(channel_id)
+        # Получаем информацию о членстве пользователя в канале
+        chat_member = await bot.get_chat_member(f"{channel_username}", user_id)
 
         # Проверяем, является ли пользователь участником канала
-        if chat_member_count > 0:
+        if chat_member.status == 'member' or chat_member.status == 'creator':
             return True
         else:
             return False
@@ -229,7 +226,7 @@ async def channels(message: types.Message):
     for i, item in enumerate(admin_channels, 1):
         text += f"\n {i}. {item}"
     for i in admin_channels:
-        subscribded = await check_user_subscription(i)
+        subscribded = await check_user_subscription(i, message.from_user.id)
         if subscribded:
             await message.answer("Вы подписаны на каналы!")
         else:
